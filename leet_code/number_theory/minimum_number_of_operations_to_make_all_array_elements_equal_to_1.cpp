fe27b08127a3fca14cae51c8453148471263eaba
GhassenHafsiaINSAT <ghassen.hafsia@insat.ucar.tn>
2024-03-22 15:29:54 +0100

    create smallest_value_after_replacing_it_with_the_smallest_value

---
+++ leet_code/number_theory/minimum_number_of_operations_to_make_all_array_elements_equal_to_1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdtr1c++.h>
+using namespace std; 
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        
+    }
+};
---
+++ leet_code/number_theory/smallest_value_after_replacing_it_with_the_smallest_value.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+class Solution {
+public:
+    int smallestValue(int n) {
+    while(n){
+        if (n == 4) break;
+        vector<int> factors; 
+        while (n > 1 && n%2 == 0){
+            factors.push_back(2);
+            n/=2;
+        }
+        
+        for(int i=3; i<=sqrt(n); i++){
+            while (n%i == 0) {
+                factors.push_back(i);
+                n/=i;
+            }
+        }
+        if (n>1) factors.push_back(n); 
+        int somme = 0;
+        for(int j=0; j<factors.size();j++){
+            somme += factors[j];
+        }
+        n = somme;
+        cout << "check "<< n << endl;
+        int nb = 0;
+        for (int k = 2; k <= sqrt(n) ; k++)
+        {
+            if (n%k ==0) nb++;
+        }
+        if (nb ==0) break;  
+        
+    }
+        return(n);
+    }
+};
+
+int main(){
+    Solution solution; 
+    int res = solution.smallestValue(4); 
+    cout << res <<endl; 
+}
